a238e81e30d1fe792ea69e301cc758bf83c1fdeb
Barreto <v.barreto.and@gmail.com>
2026-02-09 09:16:45 -0300

    Define main application class as final

--- src/main/kotlin/com/example/monitor/MonitorServiceApplication.kt
+++ src/main/kotlin/com/example/monitor/MonitorServiceApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MonitorServiceApplication
+final class MonitorServiceApplication
 
 fun main(args: Array<String>) {
-	runApplication<MonitorServiceApplication>(*args)
+    runApplication<MonitorServiceApplication>(*args)
 }
